04c96e3dd3bb55d52b2c35a36c84704911d96032
RUTEN\yurulin <yurulin@staff.ruten.com.tw>
2021-12-21 11:32:46 +0800

    [feat]axios url test

--- src/services/UserInfo.js
+++ src/services/UserInfo.js
@@ -2,18 +2,30 @@ import { $axios } from "@/api/http";
 
 const getUser = (count = 30) => {
   return new Promise((resolve, reject) => {
+    // $axios
+    //   .get(`${process.env.VUE_APP_API_ENDPOINT}/?results=${count}`)
+    //   .then((res) => {
+    //     if (res?.results) {
+    //       resolve(res.results);
+    //     } else {
+    //       res?.error ? reject("服務維護中") : reject("系統忙碌中，請稍後再試!");
+    //     }
+    //   })
+    //   .catch(() => {
+    //     reject("系統忙碌中，請稍後再試!");
+    //   });
     $axios
-      .get(`${process.env.VUE_APP_API_ENDPOINT}/?results=${count}`)
-      .then((res) => {
-        if (res?.results) {
-          resolve(res.results);
-        } else {
-          res?.error ? reject("服務維護中") : reject("系統忙碌中，請稍後再試!");
-        }
-      })
-      .catch(() => {
-        reject("系統忙碌中，請稍後再試!");
-      });
+    .get(`https://randomuser.me/api/?results=${count}`)
+    .then((res) => {
+      if (res?.results) {
+        resolve(res.results);
+      } else {
+        res?.error ? reject("服務維護中") : reject("系統忙碌中，請稍後再試!");
+      }
+    })
+    .catch(() => {
+      reject("系統忙碌中，請稍後再試!");
+    });
   });
 };
 
