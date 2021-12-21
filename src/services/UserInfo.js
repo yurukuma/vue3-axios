bb720f2ce5430622bebce9508ea3b4f74d259fad
yuru <yurulin056@gmail.com>
2021-12-21 22:28:06 +0800

    [feat]add test console

--- src/services/UserInfo.js
+++ src/services/UserInfo.js
@@ -2,20 +2,8 @@ import { $axios } from "@/api/http";
 
 const getUser = (count = 30) => {
   return new Promise((resolve, reject) => {
-    // $axios
-    //   .get(`${process.env.VUE_APP_API_ENDPOINT}/?results=${count}`)
-    //   .then((res) => {
-    //     if (res?.results) {
-    //       resolve(res.results);
-    //     } else {
-    //       res?.error ? reject("服務維護中") : reject("系統忙碌中，請稍後再試!");
-    //     }
-    //   })
-    //   .catch(() => {
-    //     reject("系統忙碌中，請稍後再試!");
-    //   });
     $axios
-      .get(`/api/?results=${count}`)
+      .get(`${process.env.VUE_APP_API_ENDPOINT}/?results=${count}`)
       .then((res) => {
         if (res?.results) {
           console.log("call api");
@@ -28,6 +16,20 @@ const getUser = (count = 30) => {
       .catch(() => {
         reject("系統忙碌中，請稍後再試!");
       });
+    // $axios
+    //   .get(`/api/?results=${count}`)
+    //   .then((res) => {
+    //     if (res?.results) {
+    //       console.log("call api");
+    //       resolve(res.results);
+    //     } else {
+    //       console.log("error");
+    //       res?.error ? reject("服務維護中") : reject("系統忙碌中，請稍後再試!");
+    //     }
+    //   })
+    //   .catch(() => {
+    //     reject("系統忙碌中，請稍後再試!");
+    //   });
   });
 };
 
