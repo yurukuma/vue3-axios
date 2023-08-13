892b21085b6c50553bfd1c5bee9573089a207cad
kuma <kuma@gmail.com>
2023-08-13 21:03:36 +0800

    feat: update package version

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-};
+module.exports =    {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "node": "current"
+        }
+      }
+    ]
+  ]
+}
